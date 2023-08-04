110959f94485860069904cd03047f21624fb6e8f
keji <tomitoyo.93@gmail.com>
2023-08-04 13:39:58 +0100

    sign up

--- screens/signUp.js
+++ screens/signUp.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import {View,Text} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Modal,
+  FlatList,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+import { COLORS, SIZES, FONTS, icons, images } from "../constants";
 
 const SignUp = () => {
+  function renderButton() {
+    return (
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2,
+        }}
+        onPress={() => console.log("Press")}
+      >
+        <Image
+          source={icons.back}
+          style={{ width: 20, height: 20, tintColor: COLORS.white }}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+    );
+  }
   return (
-    <View>
-        <Text>SignUp</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      style={{ flex: 1 }}
+    >
+      <LinearGradient
+        colors={[COLORS.lime, COLORS.emerald]}
+        style={{ flex: 1 }}
+      >
+        <ScrollView></ScrollView>
+      </LinearGradient>
+    </KeyboardAvoidingView>
+  );
+};
 
-export default SignUp
+export default SignUp;
