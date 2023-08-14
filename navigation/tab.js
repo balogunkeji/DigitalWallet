d73d1b6213ce3a1c91efdd07ac2602ab926c78d2
keji <tomitoyo.93@gmail.com>
2023-08-14 18:37:35 +0100

    add

--- navigation/tab.js
+++ navigation/tab.js
@@ -1,22 +1,84 @@
-import React from 'react';
-import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import {createBottomTabNavigator, BottomTabBar} from '@react-navigation/bottom-tabs';
-import {Home, Scan} from '../screens';
-import { COLORS, icons } from '../constants';
-import Svg, { Path } from 'react-native-svg';
-import {isIphoneX} from 'react-native-iphone-x-helper'
+import React from "react";
+import { View, Image, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+} from "@react-navigation/bottom-tabs";
+import { Home, Scan } from "../screens";
+import { COLORS, icons } from "../constants";
+import Svg, { Path } from "react-native-svg";
+import { isIphoneX } from "react-native-iphone-x-helper";
 
 const Tab = createBottomTabNavigator();
 
-
 const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={Home}/>
-      <Tab.Screen name='Scan' component={Scan}/>
-      <Tab.Screen name='User' component={Home}/>
+    <Tab.Navigator 
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: 'transparent',
+          elevation: 0
+        }
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.more}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.white : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Scan"
+        component={Scan}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.scan}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.white : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={icons.user}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.white : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 export default Tabs;
