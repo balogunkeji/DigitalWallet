36300ee1e388c7aeb060056fd26b8488df6959c1
keji <tomitoyo.93@gmail.com>
2023-08-15 11:28:18 +0100

    add

--- navigation/tab.js
+++ navigation/tab.js
@@ -23,7 +23,7 @@ const Tabs = () => {
           bottom: 0,
           left: 0,
           right: 0,
-          backgroundColor: "transparent",
+          backgroundColor: "red",
           elevation: 0,
       
       }}
